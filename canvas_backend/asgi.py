8967a5f3e4d0cad30aeb91763dd51a74b2e304f3
Orion-W <oawarawa@gmail.com>
2025-09-09 18:35:43 -0600

    Adding websockets

--- canvas_backend/asgi.py
+++ canvas_backend/asgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canvas_backend.settings')
 
-application = get_asgi_application()
+# Import routing after Django setup
+django_asgi_app = get_asgi_application()
+
+from canvas_api import routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
