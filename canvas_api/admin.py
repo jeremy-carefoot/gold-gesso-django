c1ac51665aa347908a1adb5e1551e2720dab4196
Orion-W <oawarawa@gmail.com>
2025-09-19 14:03:32 -0600

    Added duplicate action to django admin

--- canvas_api/admin.py
+++ canvas_api/admin.py
@@ -3,4 +3,17 @@ from .models import Course, Assignment
 
 # Register your models here.
 admin.site.register(Course)
-admin.site.register(Assignment)
+# admin.site.register(Assignment)
+
+@admin.action(description="Duplicate selected assignments and assign duplicated to current user.")
+def duplicate_assignments(modeladmin, request, queryset):
+    for assignment in queryset:
+        assignment.id = None
+        assignment.user_ref = request.user
+        assignment.save()
+
+class AssignmentAdmin(admin.ModelAdmin):
+    actions=[duplicate_assignments]
+
+admin.site.register(Assignment, AssignmentAdmin)
+
