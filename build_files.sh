0ab2f455a585af3c3e0599b9d83a88aa2595930c
Jeremy Carefoot <jeremycarefoot@gmail.com>
2025-10-10 12:59:10 -0600

    Add collect static to build command

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 # Build files script for deployment on Vercel
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py migrate
+
+# static files
+python3.12 manage.py collectstatic --noinput 
