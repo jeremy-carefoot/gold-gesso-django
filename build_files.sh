15c1e4e6787fff9e1caa9edc740350f2e1c82a8c
Jeremy <31494849+jeremy-carefoot@users.noreply.github.com>
2026-09-03 18:22:26 -0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
 # Build files script for deployment on Vercel
+
+# Setup Python venv
+python3.12 -m venv .venv
+source .venv/bin/activate
+
+# Install dependencies
 python3.12 -m pip install -r requirements.txt
+
+# Perform migrations
 python3.12 manage.py migrate
 
 # static files
